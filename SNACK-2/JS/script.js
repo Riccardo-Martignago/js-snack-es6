b9eb4e2962ef24670e9907e8f96c0321f72be1e0
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-23 20:30:29 +0200

    Creata lista e lista con nomi in maiuscolo

--- SNACK-2/JS/script.js
+++ SNACK-2/JS/script.js
@@ -1,1 +1,44 @@
-console.log('Hello!')
+const studenti = [
+    {
+        Id: '213',
+        Name: 'Giuseppina della Rovere',
+        Grades: '78'
+    },
+    {
+        Id: '110',
+        Name: 'Paola Cortellessa ',
+        Grades: '96'
+    },
+    {
+        Id: '250',
+        Name: 'Andrea Mantegna',
+        Grades: '48'
+    },
+    {
+        Id: '145',
+        Name: 'Gaia Borromini',
+        Grades: '74'
+    },
+    {
+        Id: '196',
+        Name: 'Luigi Grimaldello',
+        Grades: '68'
+    },
+    {
+        Id: '102',
+        Name: 'Piero della Francesca',
+        Grades: '50'
+    },
+    {
+        Id: '120',
+        Name: 'Francesca da Polenta',
+        Grades: '84'
+    }
+];
+
+const upperCaseNames = studenti.map(student => ({
+    Id: student.Id,
+    Name: student.Name.toUpperCase(),
+    Grades: student.Grades
+}));
+console.log(upperCaseNames);
