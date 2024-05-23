fc4f32c8be52a86aec003af37aff2fc173467d08
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-23 17:54:55 +0200

    Creato ciclo per trasformare le stringhe in oggetti e aggiunto le proprieta del tavolo e posto a sedere

--- SNACK-1/JS/script.js
+++ SNACK-1/JS/script.js
@@ -1,1 +1,21 @@
-console.log('Hello!')
+const invitati = [ 
+    'Brad Pitt', 
+    'Johnny Depp', 
+    'Lady Gaga', 
+    'Cristiano Ronaldo', 
+    'Georgina Rodriguez', 
+    'Chiara Ferragni',  
+    'George Clooney', 
+    'Amal Clooney', 
+    'Fedez', 
+    'Amadeus', 
+    'Fiorello'
+]
+console.log(invitati)
+let i = 0
+const invitatiObj = invitati.map((prodotto)=> {
+    i++;
+    return {tableName: "Tavolo Vip", name: prodotto, place: i};
+})
+console.log(invitatiObj);
+
