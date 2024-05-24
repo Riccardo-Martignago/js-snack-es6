ca269436edd22403a59cd1d4bf6f37fe9b289c0d
Riccardo-Martignago <martigriccardo97@gmail.com>
2024-05-24 20:12:05 +0200

    Svolto SNACK-3 estrapolando il nome della bici con il peso minore e il peso

--- SNACK-3/JS/script.js
+++ SNACK-3/JS/script.js
@@ -1,1 +1,59 @@
-console.log('hello!')
+const biciDaCorsa = [
+    { 
+        nome: "Speedster 3000",
+        peso: 7.2
+    },
+    {
+        nome: "RoadRunner Pro",
+        peso: 6.8
+    },
+    {
+        nome: "WindCatcher Elite",
+        peso: 7.1
+    },
+    { 
+        nome: "HillClimber 500", 
+        peso: 7.5
+    },
+    { 
+        nome: "SprintMaster X", 
+        peso: 6.9
+    },
+    {
+        nome: "TurboLight S",
+        peso: 7.0
+    },
+    { 
+        nome: "AeroBlade Z", 
+        peso: 7.3
+    },
+    { 
+        nome: "MountainKing", 
+        peso: 7.6
+    },
+    {
+        nome: "Veloce Racer",
+        peso: 6.7
+    },
+    {
+        nome: "UltraSpeed 400",
+        peso: 6.6
+    }
+];
+let somma = 0;
+biciDaCorsa.forEach((element) => {
+    let {peso} = element;
+    somma = somma + peso
+});
+biciDaCorsa.forEach((element) => {
+    let {peso} = element;
+    if(somma > peso){
+        somma = peso
+    }
+});
+biciDaCorsa.filter((element) => {
+    let {nome,peso} = element;
+    if(somma >= peso){
+        console.log(`${nome}, ${peso}`)
+    }
+});
